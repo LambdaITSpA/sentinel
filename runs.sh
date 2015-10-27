49b75c073c5029917ef04f8c77fa0f7448ad51dd
Claudio Guerra <claudevandort@gmail.com>
2015-10-27 17:37:07 -0300

    add runs.sh

---
+++ runs.sh
@@ -0,0 +1,11 @@
+docker rm -f sentinel
+docker run --net="host" \
+-v $PWD/docker/rails/default:/etc/apache2/sites-available/default:ro \
+-v $PWD/docker/rails/clientsentinel.cl:/etc/apache2/sites-enabled/clientsentinel.cl:ro \
+--add-host clientsentinel.cl:127.0.0.1 \
+--add-host *.clientsentinel.cl:127.0.0.1 \
+-v $PWD/../../master/sentinel:/home/ubuntu/webapp \
+--name sentinel -d lambdait/sentinel
+#-v $PWD/docker/rails/sinbad.cl.conf:/etc/apache2/sites-enabled/sinbad.cl:ro \
+#--add-host sinbad.cl:127.0.0.1 \
+#--add-host www.sinbad.cl:127.0.0.1 \
