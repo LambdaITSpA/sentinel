616b74706ca4e63aaae47a273f1cb33617ddf81b
Claudio Guerra <claudevandort@gmail.com>
2015-10-28 18:50:24 -0300

    upload previous MVP /alpha

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,24 @@
 class WelcomeController < ApplicationController
   def index
-  	
+  end
+
+  def alpha
+  end
+
+  def check_id
+  	#it's ugly as fvck I know
+
+  	#get vars
+  	headers['Access-Control-Allow-Origin'] = '*'
+	rut = params[:rut]
+	serial = params[:serial]
+
+	#query civil registry
+  	registro_civil_url = "https://portal.sidiv.registrocivil.cl/docstatus?RUN=#{rut}&type=CEDULA&serial=#{serial}"
+    doc = Nokogiri::HTML(open(registro_civil_url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}))
+    valid_chilean_id = doc.css('#tableResult > tbody > tr > td.setWidthOfSecondColumn').text == 'Vigente'
+    
+    #respond
+    render json: { valid_chilean_id: valid_chilean_id }
   end
 end
